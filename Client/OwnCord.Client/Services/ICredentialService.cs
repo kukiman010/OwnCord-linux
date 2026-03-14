6de2764cf72a5eee45682bb9ab3a9b825220d683
jevb <jevb@telenet.be>
2026-03-15 00:31:56 +0100

    fix: include SavePassword/LoadPassword/DeletePassword in ICredentialService interface

--- Client/OwnCord.Client/Services/ICredentialService.cs
+++ Client/OwnCord.Client/Services/ICredentialService.cs
@@ -5,4 +5,8 @@ public interface ICredentialService
     void SaveToken(string host, string username, string token);
     string? LoadToken(string host, string username);
     void DeleteToken(string host, string username);
+
+    void SavePassword(string host, string username, string password);
+    string? LoadPassword(string host, string username);
+    void DeletePassword(string host, string username);
 }
